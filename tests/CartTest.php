fff153ef1fdd97e0b3c92f52890ff2cf155dd89c
Anam hossain <anam@hotmail.com.au>
2017-10-23 20:07:06 +1100

    Get item by id test

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -161,5 +161,29 @@ class CartTest extends TestCase
         ]);
 
         $this->assertEquals(2, $cart->items()->count());
+        $this->assertEquals(2, $cart->getItems()->count());
+    }
+
+    public function testGetItemById()
+    {
+        $cart = new Cart('test', new MockArraySessionStorage());
+
+        $item = [
+            'id' => 123,
+            'name' => 'T-shirt',
+            'price' => 50,
+            'quantity' => 2
+        ];
+
+        $cart->add($item);
+
+        $cart->add([
+            'id' => 124,
+            'name' => 'Shoes',
+            'price' => 500,
+            'quantity' => 1
+        ]);
+
+        $this->assertEquals($item, (array) $cart->get(123));
     }
 }
